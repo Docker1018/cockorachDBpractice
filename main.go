83e70efa9c838be3ef503ff2284c4934c91e72c6
Docker <devil871018@gmail.com>
2021-06-21 13:50:50 +0800

    add get api no db

--- main.go
+++ main.go
@@ -2,10 +2,22 @@ package main
 
 import (	
 	"cockroach/controller"
+	"github.com/gin-gonic/gin"
+	"net/http"
 )
 
 func main() {
 	controller.SelectData()
 	// db.Action()
 	// fmt.Println("?");
+	router := gin.Default()
+
+	router.GET("/test/:name", getTest)
+
+	router.Run(":8085")
+}
+
+func getTest(c *gin.Context) {
+	name := c.Param("name")
+		c.String(http.StatusOK, "Hello %s", name)
 }
